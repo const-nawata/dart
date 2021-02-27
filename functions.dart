4f88087a0c08d5ac8e07cbf3ecd2b2fcd3309e6e
Kostiantyn <ckolenchenko@yukon.cv.ua>
2021-02-27 11:58:43 +0200

    functions.dart. Stop Time 21:43+

--- functions.dart
+++ functions.dart
@@ -65,30 +65,6 @@ void main() {
   print(hello2('Bob', 'good bye'));
   print(hello2('Bob', 'good bye', 'Megaphone'));
 
-  // Parameters by default
-  String trueName({String name='Bob', bool correct=false}){
-    return correct ? 'Name is $name' : 'Wrong name';
-  }
-
-  print(trueName());
-  print(trueName(correct: true));
-  print(trueName(name: 'John', correct: true));
-
-
-  // Positional parameters by default. [] -> Means "optional"
-  String hello2(String name, String msg, [String device='Dudka']){
-    var result = '$name tells $msg';
-
-    if(device != null){
-      result = '$result by $device';
-    }
-
-    return result;
-  }
-
-  print(hello2('Bob', 'good bye'));
-  print(hello2('Bob', 'good bye', 'Megaphone'));
-
 
   createListAndMap(
       {List<int> list = const[1,2,3],
